e17acf729c3bbf80437ab2a4a1ba887679f7c37f
2415035 <i2415035@continental.edu.pe>
2025-08-29 18:02:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -72,10 +72,6 @@ def process():
     })
 
 
-#if __name__ == "__main__":
-#    app.run(port=5003)
+if __name__ == "__main__":
+    app.run(port=5003)
 
-if __name__ == '__main__':
-    from flask_ngrok import run_with_ngrok
-    run_with_ngrok(app)
-    app.run()
