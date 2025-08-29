f7bdb4e6644545f423e280cbde1186c89c0aa761
2415035 <i2415035@continental.edu.pe>
2025-08-29 11:49:57 -0500

    Updateeee app.py

--- app.py
+++ app.py
@@ -74,5 +74,5 @@ def process():
 from flask_ngrok import run_with_ngrok
 
 if __name__ == "__main__":
-    run_with_ngrok(app)  # ✅ Abre el túnel ngrok automáticamente
-    app.run(port=5001)
+    #run_with_ngrok(app)  # ✅ Abre el túnel ngrok automáticamente
+    app.run()
