8c1fb79c904c0520f27a2180141a370c1d30df45
2415035 <i2415035@continental.edu.pe>
2025-09-25 15:12:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -117,5 +117,14 @@ def graficos():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@app.route("/datos")
+def datos():
+    try:
+        response = supabase.table("predictions").select("predicciones").execute()
+        registros = response.data
+        return jsonify(registros)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
